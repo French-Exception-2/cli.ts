02058654058a9b0424a1cefd125f8430417f5408
Stephane Erard <stephane.erard.se@gmail.com>
2021-04-22 12:14:12 +0200

    Split handler and api

--- src/commands/vagrant/machine/name.ts
+++ src/commands/vagrant/machine/name.ts
@@ -5,10 +5,18 @@ interface VagrantMachineNameArgv {
     "config-env": string
 }
 
+interface VagrantMachineNameRequest extends VagrantMachineNameArgv {
+
+}
+
+interface VagrantMachineNameResponse extends VagrantMachineNameArgv {
+
+}
+
 (() => {
     exports.command = 'vagrant:machine:name <name>';
     exports.desc = 'Vagrant name machine';
-    exports.builder = ((processCwd : string) => {
+    exports.builder = ((processCwd: string) => {
         const builder = {
             name: {
                 type: "string",
@@ -30,7 +38,9 @@ interface VagrantMachineNameArgv {
 
         return builder;
     })(process.cwd());
-    exports.handler = async function (argv: VagrantMachineNameArgv) {
+    exports.api = async function (request: VagrantMachineNameRequest, response: VagrantMachineNameResponse) {
+        response = response || {};
+
         const path = require('path');
         const fs = require('fs-extra');
         const _json = require('./../../../Serialization');
@@ -39,23 +49,29 @@ interface VagrantMachineNameArgv {
         const cp = require('child_process');
 
         const config = await require('@frenchex/config-api').fromFile({
-            env: { env: argv["config-env"] },
-            file: path.join(argv.path, 'config.json'),
-            root: argv.path
+            env: { env: request["config-env"] },
+            file: path.join(request.path, 'config.json'),
+            root: request.path
         });
 
-        const hostnamePattern = await config.get('nodes.' + argv.name + '.hostname-pattern', null);
-        
+        const hostnamePattern = await config.get('nodes.' + request.name + '.hostname-pattern', null);
+
         if (typeof hostnamePattern != 'string') {
-            throw new Error('Machine ' + argv.name + ' cannot be found');
+            throw new Error('Machine ' + request.name + ' cannot be found');
         }
 
-        const vagrant_machine_instance_str = sprintf('%02d', argv.instance);
+        const vagrant_machine_instance_str = sprintf('%02d', request.instance);
         const vagrant_machine_str = hostnamePattern
-            .replace(/\#\{NAME\}/, argv.name)
+            .replace(/\#\{NAME\}/, request.name)
             .replace(/\#\{INSTANCE\}/, vagrant_machine_instance_str);
 
-        console.log(vagrant_machine_str);
+        response.name = vagrant_machine_str;
+
+        return response;
+    };
+    exports.handler = async function (argv: VagrantMachineNameArgv) {
+        const response = await exports.api(argv);
+        console.log(response.name);
     };
 
 })();
